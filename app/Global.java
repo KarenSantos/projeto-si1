f9ebfa490bb515a473f21111deffef07aa7c75c1
KarenSantos <karensantosrocha@gmail.com>
2014-03-22 02:27:23 -0300

    paginas de erro ok

--- app/Global.java
+++ app/Global.java
@@ -1,16 +1,23 @@
 
 import play.*;
 import play.mvc.*;
+import play.mvc.Http.*;
+import play.libs.F.*;
+
 import static play.mvc.Results.*;
 
 public class Global extends GlobalSettings {
 
-//	public Result onError(Throwable t) {
-//		return internalServerError(views.html.error(t));
-//	}
-//
-//	public Result onActionNotFound(String uri) {
-//		return notFound(views.html.error("Ops! A página " + uri
-//				+ " não foi encontrada no nosso app. Tente outra coisa =)."));
-//	}
+	public Promise<SimpleResult> onError(RequestHeader request, Throwable t) {
+		return Promise.<SimpleResult>pure(internalServerError(views.html.error.render(t + "erro")));
+    }
+	
+	public Promise<SimpleResult> onHandlerNotFound(RequestHeader request) {
+		return Promise.<SimpleResult>pure(notFound(views.html.error.render("Ops! A página \"" + request.uri()
+				+ "\" não foi encontrada no nosso app. Tente outra coisa =).")));
+	}
+	
+	public Promise<SimpleResult> onBadRequest(RequestHeader request, String error) {
+        return Promise.<SimpleResult>pure(badRequest("Don't try to hack the URI!"));
+    }
 }
